2a25e296d958362862770c18f149fb0833fb18f4
Jefter Rocha <32802064+JefterRocha@users.noreply.github.com>
2019-06-17 10:30:17 -0300

    Add files via upload

---
+++ precache-manifest.b7ddabe95797d85d7f7c8118a89c6d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9306f724db830853cf6896c13c3abd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b984baa66c31649f4f7",
+    "url": "/static/css/main.9f61876c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2b984baa66c31649f4f7",
+    "url": "/static/js/main.7f5a90df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/static/media/Montserrat-Regular.a8a11736.ttf"
+  }
+]);
